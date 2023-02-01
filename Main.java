b250d2e864d7333658cbbad38f3fe58e7cae48c1
Leo-Mastrangelo <109768179+Leo-Mastrangelo@users.noreply.github.com>
2023-02-01 18:25:29 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,8 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        suma(10, 20, 50);
+        coche miCoche = new coche();
+        miCoche.AumentarPuerta();
+        System.out.println(miCoche.puertas);
+
     }
-    public static void suma(int a, int b, int c) {
-        System.out.println(a + b + c);
+static class coche {
+     public int puertas= 4;
+
+     public void AumentarPuerta() {
+             this.puertas++;
     }
 }
+}
+
+
